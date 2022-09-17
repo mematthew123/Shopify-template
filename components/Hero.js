f99de40754d74ee4f7c81e74c49ef668edd183b1
Matthew R <matthew.rhoads@junolive.co>
2022-09-16 20:43:08 -0600

    removed code

--- components/Hero.js
+++ components/Hero.js
@@ -3,12 +3,6 @@ import { Fragment } from "react";
 import { Popover, Transition } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 
-const navigation = [
-  { name: "Product", href: "#" },
-  { name: "Features", href: "#" },
-  { name: "Marketplace", href: "#" },
-  { name: "Company", href: "#" },
-];
 
 export default function Hero() {
   return (
