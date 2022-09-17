e4ae55bd1d4416f3ff246a0d1b90f698c86cafdf
Matthew R <matthew.rhoads@junolive.co>
2022-09-16 20:38:23 -0600

    cleaned up hero.js

--- pages/index.js
+++ pages/index.js
@@ -15,8 +15,8 @@ export default function Home({ products }) {
           content="text/html; charset=ISO-8859-1"
         />
         <meta property="og:type" content="website" />
-        <meta property="og:title" content="ACESP 365 Swag shop" />
-        <meta property="og:description" content="ACESP 365 Swag shop" />
+        <meta property="og:title" content="ACEP 365 Swag shop" />
+        <meta property="og:description" content="ACEP 365 Swag shop" />
         <meta property="og:locale" content="en_US" />
       </Head>
       <Hero />
